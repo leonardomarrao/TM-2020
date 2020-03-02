bc6de0abf64295b865850c67b49d846e47fb37c9
leonardomarrao <Letbren3>
2020-03-02 15:53:22 +0000

    LeonardoMarrão-21525

---
+++ Aula 2/exercicio1/LeonardoMarrão-21525/script.js
@@ -0,0 +1,7 @@
+function lerInput() {
+    var texto = document.getElementById("numero").value;
+    if(!texto.length) alert("Sem conteudo no Input");
+    document.getElementById("texto").innerText = texto;
+
+
+}
