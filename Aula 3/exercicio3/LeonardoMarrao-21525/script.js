a09c8e9520d6977fa251ae5cd1276ed4be2ea862
leonardomarrao <Letbren3>
2020-03-09 14:56:39 +0000

    LeonardoMarrao-21525

---
+++ Aula 3/exercicio3/LeonardoMarrao-21525/script.js
@@ -0,0 +1,36 @@
+function changeColors() {
+    var titulosVar = document.getElementsByClassName("titulo");
+    for( let titulo of titulosVar) {
+        titulo.style.color = "red";
+    }
+}
+
+function getInfo() {
+    var link = document.getElementById("idLink");
+    var lista = document.getElementById("info");
+
+    lista.innerHTML =
+        "ID:" + link.id + "<br>" +
+        "Target:" + link.target + "<br>" +
+        "Type:" + link.type + "<br>" +
+        "Href:" + link.href + "<br>";
+
+}
+
+function setClick() {
+    var cells = document.getElementsByTagName("td");
+    console.log(cells)
+    for (var i=0; i<cells.length; i++) {
+        cells[i].onclick= function() {
+            this.innerText = clickTd()
+            this.style.backgroundColor = "green";
+        }
+    }
+}
+
+function clickTd() {
+    return prompt("novo valor");
+}
+
+setClick();
+//setTimeout(()=>setClick(), 0);
