10eb27eb5597fd1035745904c79cba5fd24d0720
leonardomarrao <leonardomarrao@ipvc.pt>
2020-03-22 21:04:48 +0000

    LeonardoMarrao-21525
    exercicio 2

---
+++ Aula 4/LeonardoMarrao-21525/TMBallTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+    public Text countText;
+    private int count;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
