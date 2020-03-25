b712bfd6ff69e32592313bbfa841af5ace50a1fb
leonardomarrao <leonardomarrao@ipvc.pt>
2020-03-25 20:07:58 +0000

    LeonardoMarrão-21525

---
+++ PongGame-LeonardoMarrao-21525/Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ballTag"))
+        {
+            GameObject.Find("gameManagerObj").GetComponent<Manager>().PlayerScored();
+        }
+
+    }
+
+
+}
---
+++ PongGame-LeonardoMarrao-21525/Assets/Scripts/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+
+    [Header("Score UI")]
+    public GameObject playertext;
+
+    private int playerScore;
+
+    public void PlayerScored()
+    {
+        playerScore++;
+        playertext.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        ball.GetComponent<Ball>().increaseSpeed();
+    }
+
+    public void Reset()
+    {
+        playerScore = 0;
+        playertext.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+    }
+}
