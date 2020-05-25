d4cb1782c28ed74af2446e481abd33fcd4adf457
leonardomarrao <leonardomarrao@ipvc.pt>
2020-05-25 16:29:49 +0100

    LeonardoMarrao-21525

--- Aula 12 - Phaser3/LeonardoMarrao-21525/scripts/script.js
+++ Aula 12 - Phaser3/LeonardoMarrao-21525/scripts/script.js
@@ -1,150 +1,143 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+// create a new scene named "Game"
+let gameScene = new Phaser.Scene('Game');
 
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
 
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+// some parameters for our scene
+gameScene.init = function() {
+  this.playerSpeed = 1.5;
+  this.enemySpeed = 2;
+  this.enemyMaxY = 280;
+  this.enemyMinY = 80;
+}
+
+// load asset files for our game
+gameScene.preload = function() {
+
+  // load images
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('dragon', 'assets/dragon.png');
+  this.load.image('treasure', 'assets/treasure.png');
 };
 
-var game = new Phaser.Game(config);
+// executed once, after assets were loaded
+gameScene.create = function() {
 
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+  // background
+  let bg = this.add.sprite(0, 0, 'background');
 
+  // change origin to the top-left of the sprite
+  bg.setOrigin(0, 0);
 
+  // player
+  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
 
-}
- var platforms;
-var score = 0;
-var scoreText;
+  // scale down
+  this.player.setScale(0.5);
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
+  // goal
+  this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
+  this.treasure.setScale(0.6);
 
-    platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+  // group of enemies
+  this.enemies = this.add.group({
+    key: 'dragon',
+    repeat: 5,
+    setXY: {
+      x: 110,
+      y: 100,
+      stepX: 80,
+      stepY: 20
+    }
+  });
 
-    player = this.physics.add.sprite(100, 450, 'dude');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.physics.add.collider(player, platforms);
+  // scale enemies
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
 
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-    stars.children.iterate(function (child) {
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.overlap(player, stars, collectStar,
-        null, this);
+  // set speeds
+  Phaser.Actions.Call(this.enemies.getChildren(), function(enemy) {
+    enemy.speed = Math.random() * 2 + 1;
+  }, this);
 
-    scoreText = this.add.text(16, 16, 'score: 0', {
-        fontSize: '32px', fill: '#000' });
+  // player is alive
+  this.isPlayerAlive = true;
 
-    bombs = this.physics.add.group();
-    this.physics.add.collider(bombs, platforms);
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
+  // reset camera
+  this.cameras.main.resetFX();
+};
 
+// executed on every frame (60 times per second)
+gameScene.update = function() {
 
+  // only if the player is alive
+  if (!this.isPlayerAlive) {
+    return;
+  }
 
+  // check for active input
+  if (this.input.activePointer.isDown) {
 
-}
+    // player walks
+    this.player.x += this.playerSpeed;
+  }
 
-function collectStar (player, star)
-{
-    star.disableBody(true, true);
-    score += 10;
-    scoreText.setText('Score: ' + score);
+  // treasure collision
+  if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
+    this.gameOver();
+  }
 
-    if (stars.countActive(true) === 0)
-    {
-        stars.children.iterate(function (child) {
-            child.enableBody(true, child.x, 0, true, true);
-        });
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) :
-            Phaser.Math.Between(0, 400);
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  // enemy movement and collision
+  let enemies = this.enemies.getChildren();
+  let numEnemies = enemies.length;
+
+  for (let i = 0; i < numEnemies; i++) {
+
+    // move enemies
+    enemies[i].y += enemies[i].speed;
+
+    // reverse movement if reached the edges
+    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+      enemies[i].speed *= -1;
+    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+      enemies[i].speed *= -1;
     }
 
-}
-
-function hitBomb (player, bomb)
-{
-    this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play('turn');
-    gameOver = true;
-}
-
-
-
-function update ()
-{
-    cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
+    // enemy collision
+    if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+      this.gameOver();
+      break;
     }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    }
-    else
-    {
-        player.setVelocityX(0);
-        player.anims.play('turn');
-    }
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.setVelocityY(-330);
-    }
+  }
+};
 
-}
+gameScene.gameOver = function() {
+
+  // flag to set player is dead
+  this.isPlayerAlive = false;
+
+  // shake the camera
+  this.cameras.main.shake(500);
+
+  // fade camera
+  this.time.delayedCall(250, function() {
+    this.cameras.main.fade(250);
+  }, [], this);
+
+  // restart game
+  this.time.delayedCall(500, function() {
+    this.scene.restart();
+  }, [], this);
+};
+
+
+
+// our game's configuration
+let config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+// create the game, and pass it the configuration
+let game = new Phaser.Game(config);
